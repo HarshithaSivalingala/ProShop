059152859f9ce79571208e787fa4d09107f07ecb
harshitha_022 <19wh1a1253@bvrithyderabad.edu.in>
2022-09-08 22:29:06 +0530

    added es modules and dotenv file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from'./data/products.js'
 
+dotenv.config()
  const app=express()
  app.get('/',(req,res)=>{
     res.send("API is running")
@@ -13,5 +15,6 @@ const products = require('./data/products')
     const product=products.find(p=>p._id===req.params.id)
     res.json(product)
  })
+ const PORT =process.env.PORT || 5000
 
-app.listen(5000,console.log("server running"))
+app.listen(PORT,console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { Link,useParams} from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
-const ProductScreen = ({match})=> {
-    
-    const {id}=useParams();
-  // console.log(props)
-  const product = products.find((p) => p._id ===id )
+
+const ProductScreen = ()=> {
+  
+  const {id}=useParams()  
+ const [product,setProduct]=useState({})
+ useEffect(() => {
+  
+  const fetchProduct =  async()=>{
+    const {data}= await axios.get(`/api/products/${id}`)
+    setProduct(data)
+  }
+
+ fetchProduct()
+},[])
+
  
   return (
     <>
