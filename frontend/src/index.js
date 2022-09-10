2c4b0c3e4648b367c350d13656aa47d4d7286a7d
harshitha_022 <19wh1a1253@bvrithyderabad.edu.in>
2022-09-10 19:55:28 +0530

    created redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './bootstrap.min.css'
 import App from './App'
+import { Provider } from 'react-redux';
+import store from './store'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
