a19d17b18d8194a37b91baf8d7f5f034a87ce7df
harshitha_022 <19wh1a1253@bvrithyderabad.edu.in>
2022-09-13 20:12:22 +0530

    Applied redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 const App = () => {
   return (
@@ -17,6 +18,9 @@ const App = () => {
 
            <Route path='/' element={<HomeScreen/>}/>
            <Route path='/product/:id' element={<ProductScreen/>}/>
+           <Route path='/cart/:id' element={<CartScreen/>}></Route>
+           <Route path='/cart' element={<CartScreen/>}></Route>
+
             </Routes>
           </Container>
         </main>
