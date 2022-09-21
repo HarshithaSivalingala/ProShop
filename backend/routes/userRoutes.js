4ab327e909cd90a63336b5fc9b5fd11ec630f3bc
harshitha_022 <19wh1a1253@bvrithyderabad.edu.in>
2022-09-21 16:38:28 +0530

    User Profile Screen

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,10 +4,15 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 router.post("/login", authUser);
-router.route("/profile", getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 router.route("/").post(registerUser);
 
 export default router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
           <Routes>
             <Route path="/login" element={<LoginScreen />} />
             <Route path="/register" element={<RegisterScreen />} />
+            <Route path="/profile" element={<ProfileScreen />}></Route>
             <Route path="/" element={<HomeScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart/:id" element={<CartScreen />}></Route>
