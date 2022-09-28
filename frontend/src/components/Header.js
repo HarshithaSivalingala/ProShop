945827aeb1e16fc9ba72cf5e9301f37c11c6a2cc
harshitha_022 <19wh1a1253@bvrithyderabad.edu.in>
2022-09-28 22:17:38 +0530

    Searchbox bug

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
-import { logout } from "../actions/userActions";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import SearchBox from "./SearchBox";
+import { logout } from "../actions/userActions";
 
 const Header = () => {
   const dispatch = useDispatch();
+
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
+
   const logoutHandler = () => {
     dispatch(logout());
   };
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
